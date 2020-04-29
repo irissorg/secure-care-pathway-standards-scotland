6d84b4e6353d2393dbe2d221bb0e7c1ab6d2ac26
Paul <atomicjeep@gmail.com>
2020-04-29 16:50:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 // document.querySelector("span.site-title a").style.backgroundColor = "red";
-var el = document.querySelector('span.site-title a');
-// get HTML content
-// console.log(el.innerHTML);
+var el = document.querySelector('.site-title a');
 const orig = el.innerHTML;
 const newString = orig.replace("Scotland", "<span style='font-weight: 400;'>Scotland</span>");
 el.innerHTML = newString;
-// console.log(el.innerHTML);
