2036c4f5348f0e9fd1843cb773642133cf7373a0
Paul <atomicjeep@gmail.com>
2020-04-29 10:39:16 +0100

    first version

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * secure-care-pathway-standards-scotland Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package secure-care-pathway-standards-scotland
+ * @since 1.0.0
+ */
+
+/**
+ * Define Constants
+ */
+define( 'CHILD_THEME_SECURE_CARE_PATHWAY_STANDARDS_SCOTLAND_VERSION', '1.0.0' );
+
+/**
+ * Enqueue styles
+ */
+function child_enqueue_styles() {
+
+	wp_enqueue_style( 'secure-care-pathway-standards-scotland-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_SECURE_CARE_PATHWAY_STANDARDS_SCOTLAND_VERSION, 'all' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
