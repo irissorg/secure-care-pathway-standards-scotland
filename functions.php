47f6cebf787a67a2316d5e1b4a1e9b79c4ff9815
Paul Hart <atomicjeep@gmail.com>
2020-07-15 13:06:17 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -24,3 +24,17 @@ function child_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+
+
+add_filter('body_class','add_category_to_single');
+  function add_category_to_single($classes) {
+    if (is_single() ) {
+      global $post;
+      foreach((get_the_category($post->ID)) as $category) {
+        // add category slug to the $classes array
+        $classes[] = $category->category_nicename;
+      }
+    }
+    // return the $classes array
+    return $classes;
+  }
